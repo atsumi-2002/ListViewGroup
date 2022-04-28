4b86bb0270865d85603f2907a9b8ffc7922bbb49
atsumi <atsumi.amaya@tecsup.edu.pe>
2022-04-27 22:54:20 -0500

    Agregar archivos de proyecto.

---
+++ ListViewGroup/ListViewGroup/ConsoleGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ListViewGroup
+{
+    public class ConsoleGroup : ObservableCollection<Console>
+    {
+        public string Name { get; private set; }
+        public ConsoleGroup(string name):base()
+        {
+            Name = name;
+        }
+        public ConsoleGroup(string name, IEnumerable<Console> source):base(source)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ ListViewGroup/ListViewGroup/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewGroup
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<ConsoleGroup> ConsoleList { get; set; } = new ObservableCollection<ConsoleGroup>();
+        public MainPage()
+        {
+            InitializeComponent();
+            ConsoleList.Add(new ConsoleGroup("Sony", new[]
+            {
+                new Console
+                {
+                    Title = "PlayStation 5",
+                    Price = 3000
+                },
+                new Console
+                {
+                    Title = "PlayStation 4",
+                    Price = 2500
+                }
+            }));
+            ConsoleList.Add(new ConsoleGroup("Microsoft", new[]
+            {
+                new Console
+                {
+                    Title = "Xbox 360",
+                    Price = 3000
+                },
+                new Console
+                {
+                    Title = "Xbox one",
+                    Price = 2500
+                }
+            }));
+            ConsoleList.Add(new ConsoleGroup("Nintendo", new[]
+            {
+                new Console
+                {
+                    Title = "Nindendo Wii",
+                    Price = 3000
+                },
+                new Console
+                {
+                    Title = "Nintendo Switch",
+                    Price = 2500
+                }
+            }));
+            BindingContext = this;
+        }
+    }
+}
